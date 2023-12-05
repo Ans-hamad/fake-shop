f24d4166a8f730fe36229b9fbee815c11d646c55
ans hamad <ansmughal20@gmail.com>
2023-12-05 17:42:47 +0500

    UI continue

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Card from "./components/Elements/Card";
+import Head from "./components/Head/Head";
 
 const App = () => {
   return <div>
-    
+
   </div>;
 };
 
---
+++ src/components/Elements/Card.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='grid grid-cols-3 gap-4 py-5'>
+      <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <img className="p-8 rounded-t-lg" src="https://images.unsplash.com/photo-1510557880182-3d4d3cba35a5?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="product image"/>
+    </a>
+    <div className="px-5 pb-5">
+        <a href="#">
+            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
+        </a>
+        <div className="flex items-center justify-between">
+            <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+            <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+        </div>
+    </div>
+</div>
+    </div>
+  )
+}
+
+export default Card
